a82d288d908ca0638a290c4e1b2b1cc3a2134157
Bipasha Mehra <bipashamehra00@gmail.com>
2026-05-27 03:10:21 +0530

    fix: cors

--- server/index.js
+++ server/index.js
@@ -141,11 +141,11 @@ app.post("/api/fix", async (req, res) => {
 const distPath = path.join(__dirname, '../dist');
 app.use(express.static(distPath));
 
-app.get('*', (req, res) => {
-  if (req.path.startsWith('/api')) {
-    return res.status(404).json({ error: "API Route not found" });
+app.use((req, res, next) => {
+  if (req.method === 'GET' && !req.path.startsWith('/api')) {
+    return res.sendFile(path.join(distPath, 'index.html'));
   }
-  res.sendFile(path.join(distPath, 'index.html'));
+  next();
 });
 
 app.listen(port, () => {
